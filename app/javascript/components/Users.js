f55b5b79e8474464d27e378302cb6968d7aad85f
Sydney Orr <sydneywhitters@gmail.com>
2021-08-25 08:56:52 -0600

    seed and style

--- app/javascript/components/Users.js
+++ app/javascript/components/Users.js
@@ -2,20 +2,26 @@ import React from "react"
 
 const Users = (props) => {
   return (
-    <div>
-    <h1>All Users</h1>
-    <a href="/users/new">Create New User</a>
+    <div style={{color: "cadetblue"}}>
+    <h1 style={{alignItems:"center", fontFamily:'monospace'}}>ALL USERS</h1>
+    <a style={{textDecoration:'none', color:'cornflowerblue'}} href="/users/new">Create New User</a>
     {props.users.map((user) => {
-      return <div style={{margin: "20px", border:"1px solid black"}} key={user.id}>
+      return <div style={{
+        margin: "20px", 
+        border:"1px solid black", 
+        padding: "20px",
+        backgroundColor: "aliceblue",
+        }} 
+        key={user.id}>
         <p>ID:{user.id}</p>
         <p>{user.name}</p>
         <p>{user.age}</p>
         <div>
-          <a href={`/users/${user.id}`}>Show User</a>
+          <a style={{textDecoration:'none', color:'cornflowerblue'}} href={`/users/${user.id}`}>Show User</a>
           <p></p>
-          <a href={`/users/${user.id}/edit`}>Edit User</a>
+          <a style={{textDecoration:'none', color:'cornflowerblue'}} href={`/users/${user.id}/edit`}>Edit User</a>
           <p></p>
-          <a href={`/users/${user.id}`} data-method="delete">Delete User</a>
+          <a style={{textDecoration:'none', color:'cornflowerblue'}} href={`/users/${user.id}`} data-method="delete">Delete User</a>
         </div>
       </div>;
     })}
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# enable_extension "plpgsql"
+
+# create_table "users", force: :cascade do |t|
+#   t.string "name"
+#   t.integer "age"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+# end
+
+# end
+
+
+require "faker"
+
+50.times do
+c1 = User.create(name: Faker::Name.name, age: rand(10...100))
+
+puts c1
+end
